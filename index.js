134da2099553b72942d16d5290e6eedf2bb73269
Myles Borins <myles.borins@gmail.com>
2017-01-10 10:28:56 -0500

    NO CASE

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const stream = T.stream('user');
 
 stream.on('tweet', (tweet) => {
   console.log(`${tweet.user['screen_name']} says:\n ${tweet.text}\n`);
-  if (tweet.user['screen_name'] === config.username
+  if (tweet.user['screen_name'].toLowerCase() === config.username.toLowerCase()
         && tweet.text.search('#cleanup') > -1)
     deleteThread(T, tweet);
 });
