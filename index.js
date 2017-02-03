6e66e5c92ea3e27191bcaf5db382909c08764975
Myles Borins <myles.borins@gmail.com>
2017-02-03 03:18:29 -0500

    it me

--- index.js
+++ index.js
@@ -14,10 +14,15 @@ const T = new Twit(config);
 
 const stream = T.stream('user');
 
+function itMe(user) {
+  return user['screen_name'].toLowerCase() === config.username.toLowerCase()
+}
+
 stream.on('tweet', (tweet) => {
-  if (tweet.user['screen_name'].toLowerCase() === config.username.toLowerCase()) {
+  if (itMe(tweet.user)) {
     console.log(`${tweet.user['screen_name']} says:\n ${tweet.text}\n`);
-    if(config.amnesia && !!tweet['retweeted_status']) amnesia(T, tweet, config.amnesia);
+    if(config.amnesia && !!tweet['retweeted_status']
+      && !itMe(tweet['retweeted_status'].user)) amnesia(T, tweet, config.amnesia);
     if (tweet.text.search('#cleanup') > -1) deleteThread(T, tweet);
   }
 });
