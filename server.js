2215c5f223f860e1abd8f7317fc4a3075afb082f
Myles Borins <myles.borins@gmail.com>
2020-02-20 01:27:16 -0500

    moar logs

--- server.js
+++ server.js
@@ -12,12 +12,19 @@ const config = require('./local.json');
 const T = new Twit(config);
 
 stream.start(config, (event) => {
+  console.log('Event received');
   const tweets = event['tweet_create_events'];
-  if (!tweets) return;
+  if (!tweets) {
+    console.log('event ignored');
+    return;
+  };
   tweets.forEach(tweet => {   
     if (tweet.user.id == config.userid) {
       console.log(`${tweet.user['screen_name']} says:\n ${tweet.text}\n`);
-      if (tweet.text.search('#cleanup') > -1) deleteThread(T, tweet);
+      if (tweet.text.search('#cleanup') > -1) {
+        console.log('cleanup thread found')
+        deleteThread(T, tweet);
+      }
     }
   });
 });
